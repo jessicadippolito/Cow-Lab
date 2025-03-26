3c01d2377f9f7de5169bd0b58f723a671fb5d71b
jessicadippolito <jessdipp@gmail.com>
2025-03-26 18:31:25 -0400

    completed

--- cowsay.py
+++ cowsay.py
@@ -21,9 +21,17 @@ if args[1] == '-n':
     if (find_cow(name, cows)) == None:
         print(f'Could not find {name} cow!')
     else:
-        for i in range(3,len(args)-1):
+        for i in range(3,len(args)):
             print(args[i], end = ' ')
+        print()
+        cow = find_cow(name, cows)
+        print(cow.image)
 elif args[1] == '-l':
     print('Cows available: ', end = '')
     list_cows(cows)
-
+else:
+    for i in range(1, len(args)):
+        print(args[i], end=' ')
+    print()
+    cow = find_cow('heifer', cows)
+    print(cow.image)
